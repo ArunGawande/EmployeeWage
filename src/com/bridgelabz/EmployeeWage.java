fefdc1fbe6cdf9a7c172a492dec6cd1d87c96f59
Arun <arungawande1995@gmail.com>
2022-03-15 09:04:51 +0530

    Added UC-1-empwage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -4,6 +4,13 @@ public class EmployeeWage
 {
     public static void main(String[] args)
     {
-        System.out.println("Welcome To EmpWage Program");
+        //constants
+        int IS_FULL_TIME = 1;
+        //Computation
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_FULL_TIME)
+            System.out.println("employee is Present");
+        else
+            System.out.println("employee is absent");
     }
 }
