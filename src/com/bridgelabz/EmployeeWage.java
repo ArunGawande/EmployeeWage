999820847062a20afb683432eb69c77db5d46beb
Arun <arungawande1995@gmail.com>
2022-03-24 07:59:11 +0530

    Added: Compute Employee Wage for multiple Companies

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -4,11 +4,8 @@ class EmployeeWage
 {
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
-    public static final int EMP_RATE_PER_HOUR = 20;
-    public static final int NUM_OF_WORKING_DAYS = 2;
-    public static final int MAX_HRS_IN_MONTH = 10;
 
-    static void ComputeEmployeeWage()
+    public static int computeEmpWage(String company, int empRatePerHour ,int  numOfWorkingDays , int maxHoursPerMonth)
     {
 
         //Variables
@@ -17,8 +14,8 @@ class EmployeeWage
         int totalWorkingDays = 0;
 
         //Computation
-        while (totalEmpHrs <= MAX_HRS_IN_MONTH &&
-                totalWorkingDays < NUM_OF_WORKING_DAYS)
+        while (totalEmpHrs <= maxHoursPerMonth &&
+                totalWorkingDays < numOfWorkingDays)
         {
             totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
@@ -36,13 +33,16 @@ class EmployeeWage
             totalEmpHrs += empHrs;
             System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " + empHrs);
         }
-        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Total Emp Wage: " + totalEmpWage);
+        int totalEmpWage = totalEmpHrs * empRatePerHour;
+        System.out.println("Total Emp Wage for company: " + company+" is:" + totalEmpWage);
+        return totalEmpWage;
 
 
     }
     public static void main(String args[])
     {
-        ComputeEmployeeWage();
+        computeEmpWage("DMart", 20, 2, 10);
+        computeEmpWage("Reliance", 10, 4, 20);
+
     }
 }
